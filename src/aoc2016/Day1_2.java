1f741693fac24a7ca44b0cdf5add607b67828714
blueyleader <blueyleader@gmail.com>
2016-11-30 22:22:04 -0500

    its almost time
    setup for day 1

---
+++ src/aoc2016/Day1_2.java
@@ -0,0 +1,22 @@
+package aoc2016;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day1_2 {
+
+	public static void main(String[] args) throws FileNotFoundException{
+		String input;
+		File file = new File("test.txt");
+		//File file = new File("day1.txt");
+		Scanner scan = new Scanner(file);
+		input = scan.nextLine();
+		
+		//for(int x=0;x<input.length();x++){
+		while(scan.hasNext()){
+			
+		}
+		
+	}
+}
